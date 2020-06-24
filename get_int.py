cfc8f502939bdb56a4019d578ff94f931c211a6b
SrDayne <sr.dayne@gmail.com>
2020-06-24 15:46:37 +0200

    added new function

--- get_int.py
+++ get_int.py
@@ -1,6 +1,12 @@
 import subprocess
 
-process = subprocess.Popen(['iwconfig'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-out, err = process.communicate()
+def get_int():
+	iw_out = subprocess.Popen(['iwconfig'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+	out_iw, err_iw = iw_out.communicate()
 
-print(out)
+	for lines in out_iw.split('\n'):
+		if "ESSID:" in lines:
+#			print(lines.split('    ')[0])
+			return lines.split('    ')[0]
+			
+print(get_int())
